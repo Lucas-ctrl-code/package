fcbc76824e53d71b9d8e8e92f96872f3fda43c30
Lucas-ctrl-code <l07389190@gmail.com>
2025-06-19 13:29:37 +0900

    acceptance: Add new feature to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { ref, shallowRef, defineAsyncComponent } from 'vue'
 
 const HyggeHome = shallowRef(defineAsyncComponent(() => import('./components/HyggeHome.vue')))
-const HyggeMain = shallowRef(defineAsyncComponent(() => import('./components/HyggeMain.vue')))
 const HyggeDetails = shallowRef(defineAsyncComponent(() => import('./components/HyggeDetails.vue')))
 
 const route = ref(window.location.hash.replace('#/', '') || 'home')
@@ -11,7 +10,6 @@ window.addEventListener('hashchange', () => {
 })
 
 const currentComponent = () => {
-  if (route.value === 'main') return HyggeMain.value
   if (route.value === 'details') return HyggeDetails.value
   return HyggeHome.value
 }
